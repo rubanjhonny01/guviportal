7a18ae4c103ae3fe3901b941d5803e0b29617a68
rubanjhonny01 <50738042+rubanjhonny01@users.noreply.github.com>
2019-07-09 00:43:46 +0530

    Create indexmatching.py

---
+++ indexmatching.py
@@ -0,0 +1,11 @@
+n=int(input())
+l1=[]
+l=list(map(int,input().split()))
+for i in range(len(l)):
+    if i==l[i]:
+        l1.append(i)
+if l1!=[]:
+    for x in l1:
+        print(x,end=" ")
+else:
+    print(-1)
