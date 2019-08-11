1e09d4d5bffc4302262a2dc4dd421aa04a45226e
rubanjhonny01 <50738042+rubanjhonny01@users.noreply.github.com>
2019-08-11 12:37:08 +0530

    Update minstepstoconvertytox.py

--- minstepstoconvertytox.py
+++ minstepstoconvertytox.py
@@ -2,7 +2,12 @@ x,y=map(str,input().split())
 l=len(x)>=len(y) and len(y)or len(x)
 l1=len(x)>=len(y) and len(x)or len(y)
 count1=0
+s=""
 for i in range(l):
   if x[i]==y[i]:
     count1+=1
-print(l1-count1)
+  elif (x in y) or (y in x):
+    print(l1-l)
+    break
+if count1!=0:
+  print(l1-count1)
