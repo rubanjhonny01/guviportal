57fcba3c133c7e8f7c86e17c1ea0a95bc63e987e
rubanjhonny01 <50738042+rubanjhonny01@users.noreply.github.com>
2019-08-11 13:01:45 +0530

    Update find2setsareequaltoformn.py

--- find2setsareequaltoformn.py
+++ find2setsareequaltoformn.py
@@ -1,5 +1,19 @@
 n,a,b=map(int,input().split())
+sum1=0
+i=2
+j=2
 if n%(a+b)==0:
-  print("YES")
+    print("YES")
 else:
-  print("NO")
+    a1="NO"
+    while sum1<=n:
+        while sum1<=n:
+            sum1=(a*i)+(b*j)
+            if sum1==n:
+                a1="YES"
+                break
+            i+=2
+        j+=2
+        i=2
+    print(a1)
+        
