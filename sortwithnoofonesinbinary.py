71ce8b473666e0e696832fd6ed8d3f69bbcd840e
rubanjhonny01 <50738042+rubanjhonny01@users.noreply.github.com>
2019-08-12 12:00:49 +0530

    Create sortwithnoofonesinbinary.py

---
+++ sortwithnoofonesinbinary.py
@@ -0,0 +1,21 @@
+n=int(input())
+l1=list(map(int,input().split()))
+dic1={'a':1}
+l=[]
+for x in l1:
+    s=''
+    k=x
+    while x!=1:
+        if x%2==1:
+            s+='1'
+        x=x//2
+    s+='1'
+    if s not in l:
+        l.append(s)
+    dic1[k]=s
+l.sort()
+l.reverse()
+for x in l:
+    for y,v in dic1.items():
+        if x==v:
+            print(y)
