3d072cbb6e27c583f87d485c1dc52c67abab2abb
rubanjhonny01 <50738042+rubanjhonny01@users.noreply.github.com>
2019-08-12 10:19:00 +0530

    Create xorofanarray.py

---
+++ xorofanarray.py
@@ -0,0 +1,9 @@
+n,q=map(int,input().split())
+l=list(map(int,input().split()))
+for i in range(q):
+  x,y=map(int,input().split())
+  f=l[x-1:y]
+  val=1
+  for i in range(len(f)):
+    val^=f[i]
+  print(val)
